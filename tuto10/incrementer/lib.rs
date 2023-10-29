ae5f75e8d54d1907112a2be4da43872436da6a44
GuiGou <guigoucrypto@gmail.com>
2023-10-29 09:41:31 +0100

    Tuto 10 - Terminate a contract

---
+++ tuto10/incrementer/lib.rs
@@ -0,0 +1,47 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+#[ink::contract]
+mod incrementer {
+
+    #[ink(storage)]
+    pub struct Incrementer {
+        value: i32,
+    }
+
+    #[ink(event)]
+    pub struct Incremented {
+        by: i32,
+        new_value: i32,
+        who: AccountId,
+    }
+
+
+    impl Incrementer {
+
+        #[ink(constructor)]
+        pub fn new(init_value: i32) -> Self {
+            Self { value: init_value }
+        }
+
+
+        #[ink(message)]
+        pub fn inc(&mut self, by: i32) {
+            self.value += by;
+            ink::env::debug_println!("increment by {}, new value {}", by, self.value);
+            let signer = self.env().caller();
+            self.env().emit_event(Incremented{ by, new_value: self.value, who: signer });
+        }
+
+        #[ink(message)]
+        pub fn get(&self) -> i32 {
+            self.value
+        }
+
+        #[ink(message)]
+        pub fn terminate_me(&mut self) {
+            self.env().terminate_contract(self.env().caller());
+        }
+
+    }
+
+}
